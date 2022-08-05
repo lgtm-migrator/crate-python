e8b10361fe117484aed8b7f374f582f7f9c38b2c
Andreas Motl <andreas.motl@crate.io>
2022-08-05 16:23:53 +0200

    Data type converter: Add unit test to exercise more test cases

--- src/crate/client/test_cursor.py
+++ src/crate/client/test_cursor.py
@@ -19,11 +19,15 @@
 # with Crate these terms will supersede the license and you may use the
 # software solely pursuant to the terms of the relevant commercial agreement.
 
+from datetime import datetime
+from ipaddress import IPv4Address
 from unittest import TestCase
 from unittest.mock import MagicMock
 
 from crate.client import connect
+from crate.client.cursor import Cursor
 from crate.client.http import Client
+from crate.client.test_util import ClientMocked
 
 
 class CursorTest(TestCase):
@@ -44,4 +48,36 @@ class CursorTest(TestCase):
         statement = 'select * from locations where position = ?'
         c.execute(statement, bulk_parameters=[[1]])
         client.sql.assert_called_once_with(statement, None, [[1]])
+        conn.close()
+
+    def test_execute_with_converter(self):
+        client = ClientMocked()
+        conn = connect(client=client)
+
+        # Use the set of data type converters from `DefaultTypeConverter`
+        # and add another custom converter.
+        converter = Cursor.get_default_converter()
+        converter.set(25, lambda value: int(value[2:-1], 2))
+
+        # Create a `Cursor` object with converter.
+        c = conn.cursor(converter=converter)
+
+        # Make up a response using CrateDB data types `STRING`, `IP`,
+        # `TIMESTAMP`, `BIT`.
+        conn.client.set_next_response({
+            "col_types": [4, 5, 11, 25],
+            "cols": ["name", "address", "timestamp", "bitmask"],
+            "rows": [["foo", "10.10.10.1", 1658167836758, "B'0110'"]],
+            "rowcount": 1,
+            "duration": 123
+        })
+
+        c.execute("")
+        result = c.fetchone()
+        self.assertEqual(result, [
+            'foo',
+            IPv4Address('10.10.10.1'),
+            datetime(2022, 7, 18, 18, 10, 36, 758000),
+            6
+        ])
         conn.close()
