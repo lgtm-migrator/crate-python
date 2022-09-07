141054780f4c3b2257c46590f07affb192ae5332
Andreas Motl <andreas.motl@crate.io>
2022-09-08 01:27:50 +0200

    Data type converter: Exercise `None` conversion

--- src/crate/client/test_cursor.py
+++ src/crate/client/test_cursor.py
@@ -58,7 +58,7 @@ class CursorTest(TestCase):
         # Use the set of data type converters from `DefaultTypeConverter`
         # and add another custom converter.
         converter = Cursor.get_default_converter()
-        converter.set(CrateDatatypeIdentifier.BIT, lambda value: int(value[2:-1], 2))
+        converter.set(CrateDatatypeIdentifier.BIT, lambda value: value is not None and int(value[2:-1], 2) or None)
 
         # Create a `Cursor` object with converter.
         c = conn.cursor(converter=converter)
@@ -68,18 +68,29 @@ class CursorTest(TestCase):
         conn.client.set_next_response({
             "col_types": [4, 5, 11, 25],
             "cols": ["name", "address", "timestamp", "bitmask"],
-            "rows": [["foo", "10.10.10.1", 1658167836758, "B'0110'"]],
+            "rows": [
+                ["foo", "10.10.10.1", 1658167836758, "B'0110'"],
+                [None, None, None, None],
+            ],
             "rowcount": 1,
             "duration": 123
         })
 
         c.execute("")
-        result = c.fetchone()
+        result = c.fetchall()
         self.assertEqual(result, [
-            'foo',
-            IPv4Address('10.10.10.1'),
-            datetime(2022, 7, 18, 18, 10, 36, 758000),
-            6
+            [
+                'foo',
+                IPv4Address('10.10.10.1'),
+                datetime(2022, 7, 18, 18, 10, 36, 758000),
+                6,
+            ],
+            [
+                None,
+                None,
+                None,
+                None,
+            ],
         ])
 
         # When removing the converters, all values are forwarded 1:1.
